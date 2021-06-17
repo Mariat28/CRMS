713d5209369c10ecbf23e513595d92f1738ceeae
Elijahwb <brandonelijah099@gmail.com>
2021-06-17 17:18:34 +0300

    all components apart from the rigth components

--- helpdesk/src/views/Tickets.vue
+++ helpdesk/src/views/Tickets.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
- 
+    <TicketList/>
   </div>
 </template>
 <script>
 
+import TicketList from '@/components/AllTicketList.vue'
+
 export default {
   name:'AllTickets',
- 
+  components: {
+    TicketList
+  }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.content {
+    margin-left: 100px;
+}
+</style>
