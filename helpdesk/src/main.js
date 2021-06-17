9b5cdf25bb1a47f806d2408fbc91c46ad8242d00
Mariat28 <mariatndagire28@gmail.com>
2021-06-17 06:54:14 +0300

    working on sidebar

--- helpdesk/src/main.js
+++ helpdesk/src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/css/app.css';
+import './assets/css/bootstrap.min.css';
+import './assets/css/app.min.css';
+import './assets/css/icons.min.css';
+
+// import './assets/js/app.min.js';
 import App from './App.vue'
 import router from './router'
 import store from './store'
